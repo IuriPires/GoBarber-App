2756d933463b316d56fd871f7646dae58f0fc373
Iuri Pires <iuripires.work@gmail.com>
2020-06-04 18:32:01 -0300

    Vector icon instalado e configurado + Parte da tela de login + componentes: Input e Button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import {Image} from 'react-native';
 
-import {Container, Title} from './styles';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
 import LogoImg from '../../assets/logo.png';
+import {Container, Title} from './styles';
 
 const SignIn: React.FC = () => (
   <Container>
     <Image source={LogoImg} />
     <Title>Faça seu logon</Title>
+
+    <Input name="email" icon="mail" placeholder="E-Mail" />
+    <Input name="password" icon="lock" placeholder="Senha" />
+
+    <Button onPress={() => {}}>Entrar</Button>
   </Container>
 );
 
