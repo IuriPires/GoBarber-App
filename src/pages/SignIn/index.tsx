fcd9493671a7e60d868b1f00ea5762119e5d969f
Iuri Pires <iuripires.work@gmail.com>
2020-06-04 17:21:14 -0300

    Adicionada font RobotoSlab ao projeto

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Image} from 'react-native';
 
-import {Container} from './styles';
+import {Container, Title} from './styles';
 import LogoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => (
   <Container>
     <Image source={LogoImg} />
+    <Title>Faça seu logon</Title>
   </Container>
 );
 
