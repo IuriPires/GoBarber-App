2f2368cc715cc7ac92dc486feb89cf83f19dd69e
Iuri Pires <iuripires.work@gmail.com>
2020-06-05 11:10:17 -0300

    SignUp page pronta

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,22 +1,57 @@
 import React from 'react';
-import {Image} from 'react-native';
+import {
+  Image,
+  KeyboardAvoidingView,
+  ScrollView,
+  View,
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 import LogoImg from '../../assets/logo.png';
-import {Container, Title} from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => (
-  <Container>
-    <Image source={LogoImg} />
-    <Title>Faça seu logon</Title>
+  <>
+    <KeyboardAvoidingView
+      style={{flex: 1}}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled>
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{flex: 1}}>
+        <Container>
+          <Image source={LogoImg} />
+          <View>
+            <Title>Faça seu logon</Title>
+          </View>
 
-    <Input name="email" icon="mail" placeholder="E-Mail" />
-    <Input name="password" icon="lock" placeholder="Senha" />
+          <Input name="email" icon="mail" placeholder="E-Mail" />
+          <Input name="password" icon="lock" placeholder="Senha" />
 
-    <Button onPress={() => {}}>Entrar</Button>
-  </Container>
+          <Button onPress={() => {}}>Entrar</Button>
+          <ForgotPassword onPress={() => {}}>
+            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+          </ForgotPassword>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+
+    <CreateAccountButton>
+      <Icon name="log-in" size={20} color="#ff9000" />
+      <CreateAccountButtonText>Criar conta</CreateAccountButtonText>
+    </CreateAccountButton>
+  </>
 );
 
 export default SignIn;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import {
+  Image,
+  KeyboardAvoidingView,
+  ScrollView,
+  View,
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import {Container} from './styles';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
-const SignUp: React.FC = () => {
-  <Container />;
-};
+import LogoImg from '../../assets/logo.png';
+import {Container, Title, BackToSignIn, BackToSignInText} from './styles';
+
+const SignUp: React.FC = () => (
+  <>
+    <KeyboardAvoidingView
+      style={{flex: 1}}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled>
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{flex: 1}}>
+        <Container>
+          <Image source={LogoImg} />
+          <View>
+            <Title>Crie sua conta</Title>
+          </View>
+
+          <Input name="name" icon="user" placeholder="Nome" />
+          <Input name="email" icon="mail" placeholder="E-Mail" />
+          <Input name="password" icon="lock" placeholder="Senha" />
+
+          <Button onPress={() => {}}>Entrar</Button>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+
+    <BackToSignIn>
+      <Icon name="arrow-left" size={20} color="#fff" />
+      <BackToSignInText>Voltar para logon</BackToSignInText>
+    </BackToSignIn>
+  </>
+);
 
 export default SignUp;
